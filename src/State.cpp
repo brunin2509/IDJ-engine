02a12a9346c3be0d15c5428fdf99b595ed1f0ed9
Bruno Rodrigues <brunin2509@gmail.com>
2018-04-07 17:17:45 -0300

    Implementacao do parallax linear e ajuste dos pinguins

--- src/State.cpp
+++ src/State.cpp
@@ -92,7 +92,7 @@ bool State::QuitRequested() {
 void State::AddObject(int mouseX, int mouseY) {
     auto gameObject = new GameObject();
 
-    auto sprite = new Sprite(*gameObject, "./assets/img/penguin.png");
+    auto sprite = new Sprite(*gameObject, "./assets/img/penguinface.png");
 
     gameObject->box.x = mouseX - gameObject->box.w/2 + Camera::pos.x;
     gameObject->box.y = mouseY - gameObject->box.h/2 + Camera::pos.y;
